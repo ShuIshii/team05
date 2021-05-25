ed86e2fac1b6bc96831a21fbe2e866b8b0c8b9bc
aoao010817 <68419905+aoao010817@users.noreply.github.com>
2021-05-25 13:14:18 +0900

    5/25

--- js/main.js
+++ js/main.js
@@ -3,17 +3,11 @@ $(function(){
     $("#o_p_loader-bg").css("display", "none");
   }, 1000);
 });
-
-//================================
-//  ! scrollで表示
-//================================
 $(function(){
-//下から表示させる要素を指定
     var winh = window.innerHeight;
     var pageheadtop = $('.o_c_title-head').height();
     var pagetop = $('.l_h-header .o_c_btn-arrow');
   $(window).on( 'scroll', function () {
-    //スクロール位置を取得
     if ( $(this).scrollTop() < pageheadtop /4 ) {
       $('.no_frontpage .l_h-header__logo').removeClass('js-show-on'); 
     } else {
@@ -23,7 +17,6 @@ $(function(){
   });
 });
 
-//クリックした時の遷移
 $(function(){
   $('.l_h-header .o_c_btn-arrow').click(function(){
     //スクロールのスピード
@@ -36,12 +29,8 @@ $(function(){
   });
 });
 
-//================================
-//  ! ハンバーガー
-//================================
+//  ハンバーガー
 $(function(){
-    console.debug('hamburger_start');
-
     $('.l_h-header__btns__btn').click(function(){
 
         $('body,main, .l-gnav, .l-gnav__bg, .l_f-footer, .o_p_nav-grand, .l_h-header__btns__btn').toggleClass('f_js-open'); 
@@ -57,12 +46,4 @@ $(function(){
         $(".l_h-header__btns__btn .open").text('MENU');
     });
     console.debug('hamburger_end');
-
-});
-
-//================================
-//  ! objectFitImages IE対策 
-//================================
-$(function(){
-  objectFitImages('img');
 });
